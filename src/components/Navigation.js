a961a858da2e9d92ac4b3999641a699a7bd5cf66
unknown <marcin.czaja2@gmail.com>
2020-07-30 22:26:37 +0200

    temporary dropdown

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,9 +5,9 @@ import logo from "../images/vault.png";
 class Navigation extends React.Component {
   state = {
     barExpand: "",
-    barOne:"",
-    barTwo:"",
-    barThree:"",
+    barOne: "",
+    barTwo: "",
+    barThree: "",
   };
   resizeNav() {
     if (window.pageYOffset < 300 && window.innerWidth > 1000) {
@@ -21,27 +21,37 @@ class Navigation extends React.Component {
     }
   }
   burgerAnimation() {
-      if(this.state.barOne === "") {
-          this.setState({
-              barOne:'barOneAnimated',
-              barTwo:'barTwoAnimated',
-              barThree:'barThreeAnimated'
-          })
-      } else {
-          this.setState({
-              barOne:'',
-              barTwo:'',
-              barThree:''
-          })
-      }
+    if (this.state.barOne === "") {
+      this.setState({
+        barOne: "barOneAnimated",
+        barTwo: "barTwoAnimated",
+        barThree: "barThreeAnimated",
+      });
+    } else {
+      this.setState({
+        barOne: "",
+        barTwo: "",
+        barThree: "",
+      });
+    }
   }
   componentDidMount() {
     window.addEventListener("scroll", this.resizeNav.bind(this));
   }
   render() {
-      const classOne = `one ${this.state.barOne}`
-      const classTwo = `two ${this.state.barTwo}`
-      const classThree = `three ${this.state.barThree}`
+    const classOne = `one ${this.state.barOne}`;
+    const classTwo = `two ${this.state.barTwo}`;
+    const classThree = `three ${this.state.barThree}`;
+
+    const dropdown = (
+      <div className="nav__drop">
+        <p>
+          This is an early design of dropdown menu that will contain the most
+          important features of this application. Exact points are still to be
+          determined. It's going to be lit! 😊
+        </p>
+      </div>
+    );
     return (
       <div className="wrapper">
         <div className="nav">
@@ -49,12 +59,18 @@ class Navigation extends React.Component {
             <img className={this.state.barExpand} src={logo} alt="vault" />
           </div>
           <div className="nav__burger">
-            <div onClick={this.burgerAnimation.bind(this)} className={this.state.barOne === "" ? `burger` : `burger burgerAnimated`}>
+            <div
+              onClick={this.burgerAnimation.bind(this)}
+              className={
+                this.state.barOne === "" ? `burger` : `burger burgerAnimated`
+              }
+            >
               <div className={classOne}></div>
               <div className={classTwo}></div>
               <div className={classThree}></div>
             </div>
           </div>
+          {this.state.barOne !== "" ? dropdown : null}
         </div>
       </div>
     );
